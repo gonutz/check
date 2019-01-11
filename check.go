20119e3d9a69235d94cd2349a45d0cfb2c21fe94
Lars <lars.scheme@gmail.com>
2019-01-11 18:37:05 +0100

    Get rid of global variable Eps

--- check.go
+++ check.go
@@ -7,24 +7,37 @@ import (
 	"unsafe"
 )
 
-// Eps is the threshold that is used to compare float and complex values. They
-// are considered equal if their absolute difference is less than or equal to
-// this value. Set Eps = 0 to compare for exact equality.
-var Eps = 1e-6
-
 // Eq compares a and b and calls Errorf on t if they differ. Values are compared
 // in a deep way, similar to reflect.DeepEqual, only that float and complex
-// values are compared using Eps.
+// values are compared using an epsilon of 1e-6.
 func Eq(t *testing.T, a, b interface{}) {
 	t.Helper()
-	if !deepEqual(a, b) {
+	EqEps(t, a, b, 1e-6)
+}
+
+// EqExact compares a and b and calls Errorf on t if they differ. Values are
+// compared in a deep way, similar to reflect.DeepEqual, float and complex
+// values must match exactly.
+func EqExact(t *testing.T, a, b interface{}) {
+	t.Helper()
+	EqEps(t, a, b, 0)
+}
+
+// EqEps compares a and b and calls Errorf on t if they differ. Values are
+// compared in a deep way, similar to reflect.DeepEqual, only that float and
+// complex values are compared using epsilon. Values are considered equal if
+// their absolute difference is less than or equal to epsilon. Set epsilon to
+// zero to compare for exact equality (or use EqExact).
+func EqEps(t *testing.T, a, b interface{}, epsilon float64) {
+	t.Helper()
+	if !deepEqual(a, b, epsilon) {
 		t.Errorf("%#v != %#v", a, b)
 	}
 }
 
 // deepEqual is a modified version of reflect.DeepEqual. deepEqual compares
-// float and complex values using Eps.
-func deepEqual(x, y interface{}) bool {
+// float and complex values using epsilon.
+func deepEqual(x, y interface{}, epsilon float64) bool {
 	if x == nil || y == nil {
 		return x == y
 	}
@@ -33,7 +46,7 @@ func deepEqual(x, y interface{}) bool {
 	if v1.Type() != v2.Type() {
 		return false
 	}
-	return deepValueEqual(v1, v2, make(map[visit]bool), 0)
+	return deepValueEqual(v1, v2, epsilon, make(map[visit]bool), 0)
 }
 
 type visit struct {
@@ -42,7 +55,7 @@ type visit struct {
 	typ reflect.Type
 }
 
-func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) bool {
+func deepValueEqual(v1, v2 reflect.Value, eps float64, visited map[visit]bool, depth int) bool {
 	if !v1.IsValid() || !v2.IsValid() {
 		return v1.IsValid() == v2.IsValid()
 	}
@@ -84,7 +97,7 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 	switch v1.Kind() {
 	case reflect.Array:
 		for i := 0; i < v1.Len(); i++ {
-			if !deepValueEqual(v1.Index(i), v2.Index(i), visited, depth+1) {
+			if !deepValueEqual(v1.Index(i), v2.Index(i), eps, visited, depth+1) {
 				return false
 			}
 		}
@@ -100,7 +113,7 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 			return true
 		}
 		for i := 0; i < v1.Len(); i++ {
-			if !deepValueEqual(v1.Index(i), v2.Index(i), visited, depth+1) {
+			if !deepValueEqual(v1.Index(i), v2.Index(i), eps, visited, depth+1) {
 				return false
 			}
 		}
@@ -109,15 +122,15 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 		if v1.IsNil() || v2.IsNil() {
 			return v1.IsNil() == v2.IsNil()
 		}
-		return deepValueEqual(v1.Elem(), v2.Elem(), visited, depth+1)
+		return deepValueEqual(v1.Elem(), v2.Elem(), eps, visited, depth+1)
 	case reflect.Ptr:
 		if v1.Pointer() == v2.Pointer() {
 			return true
 		}
-		return deepValueEqual(v1.Elem(), v2.Elem(), visited, depth+1)
+		return deepValueEqual(v1.Elem(), v2.Elem(), eps, visited, depth+1)
 	case reflect.Struct:
 		for i, n := 0, v1.NumField(); i < n; i++ {
-			if !deepValueEqual(v1.Field(i), v2.Field(i), visited, depth+1) {
+			if !deepValueEqual(v1.Field(i), v2.Field(i), eps, visited, depth+1) {
 				return false
 			}
 		}
@@ -137,7 +150,7 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 			val2 := v2.MapIndex(k)
 			if !val1.IsValid() ||
 				!val2.IsValid() ||
-				!deepValueEqual(v1.MapIndex(k), v2.MapIndex(k), visited, depth+1) {
+				!deepValueEqual(v1.MapIndex(k), v2.MapIndex(k), eps, visited, depth+1) {
 				return false
 			}
 		}
@@ -173,12 +186,12 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 	case reflect.Float32, reflect.Float64:
 		k2 := v2.Kind()
 		return (k2 == reflect.Float32 || k2 == reflect.Float64) &&
-			floatEq(v1.Float(), v2.Float())
+			floatEq(v1.Float(), v2.Float(), eps)
 	case reflect.Complex64, reflect.Complex128:
 		k2 := v2.Kind()
 		return (k2 == reflect.Complex64 || k2 == reflect.Complex128) &&
-			floatEq(real(v1.Complex()), real(v2.Complex())) &&
-			floatEq(imag(v1.Complex()), imag(v2.Complex()))
+			floatEq(real(v1.Complex()), real(v2.Complex()), eps) &&
+			floatEq(imag(v1.Complex()), imag(v2.Complex()), eps)
 	case reflect.String:
 		return v2.Kind() == reflect.String && v1.String() == v2.String()
 	case reflect.UnsafePointer:
@@ -189,11 +202,11 @@ func deepValueEqual(v1, v2 reflect.Value, visited map[visit]bool, depth int) boo
 	}
 }
 
-func floatEq(a, b float64) bool {
+func floatEq(a, b, eps float64) bool {
 	return math.IsInf(a, 1) && math.IsInf(b, 1) ||
 		math.IsInf(a, -1) && math.IsInf(b, -1) ||
 		math.IsNaN(a) && math.IsNaN(b) ||
-		abs(a-b) <= Eps
+		abs(a-b) <= eps
 }
 
 func abs(x float64) float64 {
