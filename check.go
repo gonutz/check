fd5562f7ccf5dd17df0aabb07163e1ed98ffa622
Lars <lars.scheme@gmail.com>
2019-01-11 18:27:32 +0100

    Remove unused code

--- check.go
+++ check.go
@@ -198,47 +198,6 @@ func floatEq(a, b float64) bool {
 		abs(a-b) <= Eps
 }
 
-func toUint64(x interface{}) (uint64, bool) {
-	switch x.(type) {
-	case int:
-		return uint64(x.(int)), true
-	case int8:
-		return uint64(x.(int8)), true
-	case int16:
-		return uint64(x.(int16)), true
-	case int32:
-		return uint64(x.(int32)), true
-	case int64:
-		return uint64(x.(int64)), true
-	case uint:
-		return uint64(x.(uint)), true
-	case uint8:
-		return uint64(x.(uint8)), true
-	case uint16:
-		return uint64(x.(uint16)), true
-	case uint32:
-		return uint64(x.(uint32)), true
-	case uint64:
-		return uint64(x.(uint64)), true
-	default:
-		return 0, false
-	}
-}
-
-func toFloat64(x interface{}) (float64, bool) {
-	switch x.(type) {
-	case float32:
-		return float64(x.(float32)), true
-	case float64:
-		return x.(float64), true
-	default:
-		if u, ok := toUint64(x); ok {
-			return float64(u), true
-		}
-		return 0, false
-	}
-}
-
 func abs(x float64) float64 {
 	if x < 0 {
 		return -x
