dd0ec75977cffbd0a12d6cbb7383795bb60e43a9
Lars <lars.scheme@gmail.com>
2019-04-16 11:49:42 +0200

    Refactor tests

--- check_test.go
+++ check_test.go
@@ -9,88 +9,73 @@ import (
 )
 
 type mockTester struct {
-	err string
+	err      string
+	isHelper bool
 }
 
 func (t *mockTester) Errorf(format string, a ...interface{}) {
 	t.err = fmt.Sprintf(format, a...)
 }
 
-func TestEq(t *testing.T) {
+func (t *mockTester) Helper() {
+	t.isHelper = true
+}
+
+func TestEqAndNeq(t *testing.T) {
+	// eq and neq are helper functions that let us state facts about values that
+	// are equal and not equal. eq asserts that check.Eq is true for both
+	// arguments, independent of the order (a,b or b,a) and that check.Neq is
+	// false. neq does the opposite of that.
 	eq := func(a, b interface{}) {
 		t.Helper()
-		{
-			// check Eq(a,b) prints no error
-			var tt mockTester
-			check.Eq(&tt, a, b)
-			if tt.err != "" {
-				t.Errorf("%v == %v but error for Eq was %q", a, b, tt.err)
-			}
+		var ttEqAB mockTester
+		check.Eq(&ttEqAB, a, b)
+		if ttEqAB.err != "" {
+			t.Errorf("%v == %v but error for Eq was %q", a, b, ttEqAB.err)
 		}
-		{
-			// check Eq(b,a) prints no error
-			var tt mockTester
-			check.Eq(&tt, b, a)
-			if tt.err != "" {
-				t.Errorf("%v == %v but error for Eq was %q", b, a, tt.err)
-			}
+		var ttEqBA mockTester
+		check.Eq(&ttEqBA, b, a)
+		if ttEqBA.err != "" {
+			t.Errorf("%v == %v but error for Eq was %q", b, a, ttEqBA.err)
 		}
-		{
-			// check Neq(a,b) prints an error
-			var tt mockTester
-			check.Neq(&tt, a, b)
-			if tt.err == "" {
-				t.Errorf("%v != %v but have no error for Neq", a, b)
-			}
+		var ttNeqAB mockTester
+		check.Neq(&ttNeqAB, a, b)
+		if ttNeqAB.err == "" {
+			t.Errorf("%v != %v but have no error for Neq", a, b)
 		}
-		{
-			// check Neq(b,a) prints an error
-			var tt mockTester
-			check.Neq(&tt, b, a)
-			if tt.err == "" {
-				t.Errorf("%v != %v but have no error for Neq", b, a)
-			}
+		var ttNeqBA mockTester
+		check.Neq(&ttNeqBA, b, a)
+		if ttNeqBA.err == "" {
+			t.Errorf("%v != %v but have no error for Neq", b, a)
 		}
 	}
 	neq := func(a, b interface{}) {
 		t.Helper()
-		{
-			// check Eq(a,b) prints an error
-			var tt mockTester
-			check.Eq(&tt, a, b)
-			if tt.err == "" {
-				t.Errorf("%v != %v but have no error for Eq", a, b)
-			}
+		var ttEqAB mockTester
+		check.Eq(&ttEqAB, a, b)
+		if ttEqAB.err == "" {
+			t.Errorf("%v != %v but have no error for Eq", a, b)
 		}
-		{
-			// check Eq(b,a) prints an error
-			var tt mockTester
-			check.Eq(&tt, b, a)
-			if tt.err == "" {
-				t.Errorf("%v != %v but have no error for Eq", b, a)
-			}
+		var ttEqBA mockTester
+		check.Eq(&ttEqBA, b, a)
+		if ttEqBA.err == "" {
+			t.Errorf("%v != %v but have no error for Eq", b, a)
 		}
-		{
-			// check Neq(a,b) prints no error
-			var tt mockTester
-			check.Neq(&tt, a, b)
-			if tt.err != "" {
-				t.Errorf("%v == %v but error for Neq was %q", a, b, tt.err)
-			}
+		var ttNeqAB mockTester
+		check.Neq(&ttNeqAB, a, b)
+		if ttNeqAB.err != "" {
+			t.Errorf("%v == %v but error for Neq was %q", a, b, ttNeqAB.err)
 		}
-		{
-			// check Neq(b,a) prints no error
-			var tt mockTester
-			check.Neq(&tt, b, a)
-			if tt.err != "" {
-				t.Errorf("%v == %v but error for Neq was %q", b, a, tt.err)
-			}
+		var ttNeqBA mockTester
+		check.Neq(&ttNeqBA, b, a)
+		if ttNeqBA.err != "" {
+			t.Errorf("%v == %v but error for Neq was %q", b, a, ttNeqBA.err)
 		}
 	}
 
+	// numbers
 	eq(0, 0)
 	neq(1, 0)
-	eq(nil, nil)
 	eq(int8(2), uint64(2))
 	eq(float32(1.2), float64(1.2))
 	eq(complex(1, 2), complex(1, 2))
@@ -109,43 +94,45 @@ func TestEq(t *testing.T) {
 	eq(uint64(5), uint64(5))
 	neq(uint64(999), uint64(5))
 	eq(2.0, uint64(2))
-	eq("abc", "abc")
-	neq("abc", "ABC")
 
-	var s1, s2 struct{ a int }
-	eq(s1, s1)
-	eq(s1, s2)
-	s2.a = 111
-	neq(s1, s2)
-
-	p1 := unsafe.Pointer(&s1)
-	p2 := unsafe.Pointer(&s2)
-	eq(p1, p1)
-	neq(p1, p2)
-	neq(p1, &p1)
-	eq(&p1, &p1)
-
+	// boolean values
 	eq(true, true)
 	neq(true, false)
 
+	// strings
+	eq("", "")
+	neq("abc", "ABC")
+	eq("abc", "abc")
+	eq("abc", []byte("abc"))
+	eq("abc", []rune("abc"))
+	neq("abc", "ABC")
+	neq("abc", []byte("ABC"))
+	neq("abc", []rune("ABC"))
+
+	// functions
 	eq(eq, eq)
 	neq(eq, neq)
 	var nilF func()
 	eq(nilF, nilF)
 
-	eq([]int{1, 2, 3}, []int{1, 2, 3})
-	neq([]int{1, 2, 3}, []int{1, 2})
-	neq([]int{1, 2, 3}, []int{1, 2, 4})
-	neq([]int{1, 2, 3}, []int(nil))
-	slice := []int{1, 2, 3}
-	eq(slice, slice)
-
+	// arrays
 	var ints2 [2]int
 	var ints3 [3]int
 	eq(ints2, ints2)
 	neq(ints2, ints3)
 	neq([2]int{1, 2}, [2]int{1, 3})
 
+	// slices
+	eq([]int{1, 2, 3}, []int{1, 2, 3})
+	neq([]int{1, 2, 3}, []int{1, 2})
+	neq([]int{1, 2, 3}, []int{1, 2, 4})
+	neq([]int{1, 2, 3}, []int(nil))
+	slice := []int{1, 2, 3}
+	eq(slice, slice)
+	eq([]interface{}{7.0, 6.0, 5.0}, []interface{}{7, uint8(6), 5})
+	neq([]interface{}{""}, []interface{}{5})
+
+	// maps
 	var nilMap map[int]string
 	m1 := map[int]string{1: "abc"}
 	m2 := map[int]string{1: "abc", 2: "def"}
@@ -157,6 +144,13 @@ func TestEq(t *testing.T) {
 	eq(nilMap, nilMap)
 	neq(m2, m3)
 	eq(m2, m22)
+
+	// structs and interfaces
+	var s1, s2 struct{ a int }
+	eq(s1, s1)
+	eq(s1, s2)
+	s2.a = 111
+	neq(s1, s2)
 
 	type bbb struct{}
 	type aaa struct {
@@ -184,15 +178,14 @@ func TestEq(t *testing.T) {
 	aa2.i = aer{i: 2}
 	neq(aa1, aa2)
 
-	neq([]interface{}{""}, []interface{}{5})
-	eq([]interface{}{7.0, 6.0, 5.0}, []interface{}{7, 6, 5})
-
-	eq("abc", "abc")
-	eq("abc", []byte("abc"))
-	eq("abc", []rune("abc"))
-	neq("XXX", "abc")
-	neq("XXX", []byte("abc"))
-	neq("XXX", []rune("abc"))
+	// pointers
+	p1 := unsafe.Pointer(&s1)
+	p2 := unsafe.Pointer(&s2)
+	eq(p1, p1)
+	neq(p1, p2)
+	neq(p1, &p1)
+	eq(&p1, &p1)
+	eq(nil, nil)
 }
 
 type aer struct{ i int }
@@ -236,5 +229,13 @@ func TestEqExact(t *testing.T) {
 	check.NeqExact(&tt, 1.0, 1.00000001)
 	if tt.err != "" {
 		t.Error("no error expected", tt.err)
+	}
+}
+
+func TestHelperFunctionIsDeclared(t *testing.T) {
+	var tt mockTester
+	check.Eq(&tt, 0, 0)
+	if !tt.isHelper {
+		t.Error("Eq does not declare itself as Helper()")
 	}
 }
